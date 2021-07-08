4b685acebbbdb8ef451f43ffc461643a7f0d4a38
Alex <wraithmaster10@protonmail.com>
2021-07-08 17:07:39 +1200

    Update

--- config.js
+++ config.js
@@ -6,3 +6,8 @@ const thirdColor = "#F55F44";
 // trophy colors top -> bottom
 const trophyColors = ["#C3C3C3","#F4B226","#C2907D"];
 const trophyTiers = ["platinum", "gold", "bronze"];
+//if becomes webapp, webapp will communicate with this server:
+const premioServer = "";
+// if becomes webapp, these will be dynamic:
+const guestName = "Alex";
+const vouchers = ["15% off", "$5 off", "10% off"];
